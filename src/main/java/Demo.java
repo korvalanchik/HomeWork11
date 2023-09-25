cb0625322ca7eee13e40901e9bb73d642e632eeb
korvalanchik <korvalan@yahoo.com>
2023-09-25 21:42:00 +0300

    Streaming end

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,6 +1,16 @@
+import java.util.stream.Stream;
+
 public class Demo {
     public static void main(String[] args) {
-        Streaming var1 = new Streaming();
-        var1.nonPairIndexing();
+        Streaming streaming = new Streaming();
+        streaming.nonPairIndexing();
+        streaming.upperCasing();
+        streaming.sortingArray();
+        streaming.gfg();
+        System.out.println("Task 5");
+        Stream<String> first = Stream.of("If", "cut", "a", "wings", "you", "off", "legs", "that", "die", "boredom", "it", "ha", "ha", "ha", "ha");
+        Stream<String> second = Stream.of("you", "off", "bird's", "if", "cut", "its", "too", "bird", "of", "because", "won't be able to sit");
+        Stream<String> third = Streaming.zip(first, second);
+        System.out.println(third.toList().toString().replace(",", "").replace("[", "").replace("]", ""));
     }
 }
--- src/main/java/Streaming.java
+++ src/main/java/Streaming.java
@@ -1,26 +1,23 @@
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
-
-
+import java.util.stream.Stream;
 
 public class Streaming {
+    List<nameWithNumber> names = List.of(
+            new nameWithNumber(1,"Ivan"),
+            new nameWithNumber(2,"Petro"),
+            new nameWithNumber(3,"Dmitro"),
+            new nameWithNumber(4,"Stepan"),
+            new nameWithNumber(5,"Sergiy"),
+            new nameWithNumber(6,"Valerii"),
+            new nameWithNumber(7,"Jack"),
+            new nameWithNumber(8,"Michael"));
+
+    String[] array = {"1, 4, 57", "2, 15, 3", "4, 5"};
+    public void nonPairIndexing() {
 //Метод приймає на вхід список імен. Необхідно повернути рядок вигляду
 // 1. Ivan, 3. Peter... лише з тими іменами, що стоять під непарним індексом (1, 3 тощо)
-    public void nonPairIndexing() {
-
-//        String outString = null;
-        List<nameWithNumber> names = List.of(
-                new nameWithNumber(1,"Ivan"),
-                new nameWithNumber(2,"Petro"),
-                new nameWithNumber(3,"Dmitro"),
-                new nameWithNumber(4,"Stepan"),
-                new nameWithNumber(5,"Sergiy"),
-                new nameWithNumber(6,"Valerii"),
-                new nameWithNumber(7,"Jack"),
-                new nameWithNumber(8,"Michael"));
-
-
+        System.out.println("Task 1");
         LinkedHashMap<Integer, String> map = names.stream()
                 .filter(num->num.id()%2!=0)
                     .collect(
@@ -31,13 +28,64 @@ public class Streaming {
                                        LinkedHashMap::new));
 
         map.forEach((x, y) -> System.out.println(x + ". " + y));
-
+        System.out.println("");
     }
 
-    public record nameWithNumber(Integer id, String name) {
+    public void upperCasing() {
+//    Метод приймає на вхід список рядків (можна взяти список із Завдання 1).
+//    Повертає список цих рядків у верхньому регістрі,
+//    і відсортованих за спаданням (від Z до A).
+        System.out.println("Task 2");
+        List<nameWithNumber> sortedList = names.stream()
+            .sorted(Comparator.comparing(nameWithNumber::name).reversed()).toList();
+        sortedList.forEach(x-> System.out.println(x.name().toUpperCase()));
+        System.out.println("");
+    }
 
+    public void sortingArray() {
+    //  Є масив:  ["1, 2, 0", "4, 5"]
+    //  Необхідно отримати з масиву всі числа, і вивести їх у відсортованому вигляді через кому ,, наприклад:
+    //  "0, 1, 2, 4, 5"
+        System.out.println("Task 3");
+        Stream<String> arrayStream = Arrays.stream(array);
+        Stream<Integer> intStream = arrayStream.flatMap(line -> Arrays.stream(line.split(", "))).map(Integer::valueOf).sorted();
+        System.out.println(intStream.toList().toString().replace("[","").replace("]",""));
+        System.out.println("");
+    }
+    public record nameWithNumber(Integer id, String name) { }
+    //  Використовуючи Stream.iterate, створіть безкінечний стрім випадкових чисел, але не використовуючи Math.random().
+    public void gfg() {
+        // Seed value
+        long seed = 98L; //System.currentTimeMillis();
+        // Multiplier term
+        long a = 25214903917L;
+        // Increment term
+        long c = 11L;
+        // Modulus parameter
+        long m = 2^48L;
 
-        //  constructor/getters/setters
+        // Number of Random numbers
+        // to be generated
+        int numberRandomNum = 30;
+        System.out.println("Task 4");
+
+        Stream.iterate(seed, x->((x*a)+c)%m).limit(numberRandomNum).toList().forEach((Long rnd) -> System.out.print(rnd + " "));
+        System.out.println("\n");
+    }
+
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+//    Напишіть метод public static <T> Stream<T> zip(Stream<T> first, Stream<T> second)
+//    який "перемішує" елементи зі стрімів first та second, зупиняючись тоді,
+//    коли у одного зі стрімів закінчаться елементи.
+        Iterator<T> iterator1 = first.iterator();
+        Iterator<T> iterator2 = second.iterator();
+        Stream.Builder<T> builder = Stream.builder();
+
+        while (iterator1.hasNext()&&iterator2.hasNext()) {
+            builder.add(iterator1.next()).add(iterator2.next());
         }
 
+        return builder.build();
+    }
 }
+
